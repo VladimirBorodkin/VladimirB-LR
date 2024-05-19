cd1336a795879e020c4bc65218d43ba357526ca9
VladimirBorodkin <162474340+VladimirBorodkin@users.noreply.github.com>
2024-05-19 20:57:12 +0300

    Add files via upload

---
+++ DZ#12/UC5/WebSocketBuffer.h
@@ -0,0 +1,10 @@
+#ifndef VUSER_WEBSOCKET_BUFFER_HEADER
+#define VUSER_WEBSOCKET_BUFFER_HEADER
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+char WebSocketReceive0[] = "{\"messageType\":\"hello\",\"uaid\":\"559b0fde81b044cb88f9e24d51430fad\",\"statu"
+                        "s\":200,\"use_webpush\":true,\"broadcasts\":{}}";
+long WebSocketReceiveLen0   = sizeof(WebSocketReceive0) - 1;	// (record-time: 113 bytes)
+
+#endif
