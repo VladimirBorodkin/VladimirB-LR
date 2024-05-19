3aa1164c798bfd71bd823f9d79a742004e385185
VladimirBorodkin <162474340+VladimirBorodkin@users.noreply.github.com>
2024-05-19 20:36:46 +0300

    Add files via upload

---
+++ DZ#12/UC2/Action.c
@@ -0,0 +1,554 @@
+Action()
+{
+
+	web_add_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("DNT", 
+		"1");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Sec-GPC", 
+		"1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t71.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	web_url("header.html", 
+		"URL=http://localhost:1080/WebTours/header.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/WebTours/", 
+		"Snapshot=t72.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("welcome.pl", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/WebTours/", 
+		"Snapshot=t73.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"image");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"no-cors");
+
+	web_url("hp_logo.png", 
+		"URL=http://localhost:1080/WebTours/images/hp_logo.png", 
+		"Resource=1", 
+		"RecContentType=image/png", 
+		"Referer=http://localhost:1080/WebTours/header.html", 
+		"Snapshot=t74.inf", 
+		LAST);
+
+	web_url("webtours.png", 
+		"URL=http://localhost:1080/WebTours/images/webtours.png", 
+		"Resource=1", 
+		"RecContentType=image/png", 
+		"Referer=http://localhost:1080/WebTours/header.html", 
+		"Snapshot=t75.inf", 
+		LAST);
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+/*Correlation comment - Do not change!  Original value='138964.242851357HVtHQHDpctVzzzzHtciiHpiDQtf' Name ='userSession' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		LAST);
+
+	web_url("nav.pl", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Snapshot=t76.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("home.html", 
+		"URL=http://localhost:1080/WebTours/home.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Snapshot=t77.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"image");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"no-cors");
+
+	web_url("mer_login.gif", 
+		"URL=http://localhost:1080/WebTours/images/mer_login.gif", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t78.inf", 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	/* login */
+
+	web_websocket_send("ID=0", 
+		"Buffer={\"messageType\":\"hello\",\"broadcasts\":{\"remote-settings/monitor_changes\":\"\\\"1715507829476\\\"\"},\"use_webpush\":true}", 
+		"IsBinary=0", 
+		LAST);
+
+	/*Connection ID 0 received buffer WebSocketReceive0*/
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t9.inf",
+		"Mode=HTTP",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={username}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=login.x", "Value=45", ENDITEM,
+		"Name=login.y", "Value=10", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='flights' Name ='in' Type ='ResponseBased'*/
+	web_reg_save_param_regexp(
+		"ParamName=in",
+		"RegExp=Using\\ the\\ menu\\ to\\ the\\ left,\\ you\\ can\\ search\\ for\\ new\\ (.*?)\\ to\\ book,\\ ",
+		SEARCH_FILTERS,
+		"Scope=Body",
+		"IgnoreRedirections=No",
+		LAST);
+
+	web_url("login.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/login.pl?intro=true", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t80.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("nav.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t81.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"image");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"no-cors");
+
+	web_url("itinerary.gif", 
+		"URL=http://localhost:1080/WebTours/images/itinerary.gif", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t82.inf", 
+		LAST);
+
+	web_url("in_home.gif", 
+		"URL=http://localhost:1080/WebTours/images/in_home.gif", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t83.inf", 
+		LAST);
+
+	web_url("signoff.gif", 
+		"URL=http://localhost:1080/WebTours/images/signoff.gif", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t84.inf", 
+		LAST);
+
+	web_url("flights.gif",
+		"URL=http://localhost:1080/WebTours/images/{in}.gif",
+		"Resource=1",
+		"RecContentType=image/gif",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home",
+		"Snapshot=t85.inf",
+		LAST);
+
+	/* flights */
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(7);
+
+	web_url("Search Flights Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t86.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='roundtrip' Name ='.cgifields' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=.cgifields",
+		"TagName=input",
+		"Extract=name",
+		"Type=checkbox",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='seatPref' Name ='Name' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=Name",
+		"TagName=input",
+		"Extract=name",
+		"Type=radio",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='seatType' Name ='Name_1' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=Name_1",
+		"TagName=input",
+		"Extract=name",
+		"Type=radio",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		LAST);
+
+	web_url("reservations.pl", 
+		"URL=http://localhost:1080/cgi-bin/reservations.pl?page=welcome", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"Snapshot=t87.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("nav.pl_3",
+		"URL=http://localhost:1080/cgi-bin/nav.pl?page=menu&in={in}",
+		"Resource=0",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?page=search",
+		"Snapshot=t88.inf",
+		"Mode=HTTP",
+		LAST);
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"image");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"no-cors");
+
+	web_url("home.gif",
+		"URL=http://localhost:1080/WebTours/images/home.gif",
+		"Resource=1",
+		"RecContentType=image/gif",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in={in}",
+		"Snapshot=t89.inf",
+		LAST);
+
+	web_url("button_next.gif", 
+		"URL=http://localhost:1080/WebTours/images/button_next.gif", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome", 
+		"Snapshot=t90.inf", 
+		LAST);
+
+	web_url("in_flights.gif",
+		"URL=http://localhost:1080/WebTours/images/in_{in}.gif",
+		"Resource=1",
+		"RecContentType=image/gif",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in={in}",
+		"Snapshot=t91.inf",
+		LAST);
+
+	/* flights_find */
+
+	web_add_auto_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(7);
+
+/*Correlation comment - Do not change!  Original value='000;0;05/13/2024' Name ='outboundFlight' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=outboundFlight",
+		"TagName=input",
+		"Extract=value",
+		"Name=outboundFlight",
+		"Type=radio",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		LAST);
+
+	web_submit_data("reservations.pl_2",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome",
+		"Snapshot=t92.inf",
+		"Mode=HTTP",
+		ITEMDATA,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=depart", "Value=Denver", ENDITEM,
+		"Name=departDate", "Value=05/13/2024", ENDITEM,
+		"Name=arrive", "Value=Denver", ENDITEM,
+		"Name=returnDate", "Value=05/14/2024", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name={Name}", "Value=None", ENDITEM,
+		"Name={Name_1}", "Value=Coach", ENDITEM,
+		"Name=findFlights.x", "Value=58", ENDITEM,
+		"Name=findFlights.y", "Value=14", ENDITEM,
+		"Name=.cgifields", "Value={.cgifields}", ENDITEM,
+		"Name=.cgifields", "Value={Name_1}", ENDITEM,
+		"Name=.cgifields", "Value={Name}", ENDITEM,
+		LAST);
+
+	/* flights_cont */
+
+	lr_think_time(9);
+
+	web_submit_data("reservations.pl_3",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t93.inf",
+		"Mode=HTTP",
+		ITEMDATA,
+		"Name=outboundFlight", "Value={outboundFlight}", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name={Name_1}", "Value=Coach", ENDITEM,
+		"Name={Name}", "Value=None", ENDITEM,
+		"Name=reserveFlights.x", "Value=64", ENDITEM,
+		"Name=reserveFlights.y", "Value=7", ENDITEM,
+		LAST);
+
+	/* flights_details */
+
+	lr_think_time(11);
+
+	web_submit_data("reservations.pl_4",
+		"Action=http://localhost:1080/cgi-bin/reservations.pl",
+		"Method=POST",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl",
+		"Snapshot=t94.inf",
+		"Mode=HTTP",
+		ITEMDATA,
+		"Name=firstName", "Value=Jojo", ENDITEM,
+		"Name=lastName", "Value=Bean", ENDITEM,
+		"Name=address1", "Value=", ENDITEM,
+		"Name=address2", "Value=", ENDITEM,
+		"Name=pass1", "Value=Jojo Bean", ENDITEM,
+		"Name=creditCard", "Value=", ENDITEM,
+		"Name=expDate", "Value=", ENDITEM,
+		"Name=oldCCOption", "Value=", ENDITEM,
+		"Name=numPassengers", "Value=1", ENDITEM,
+		"Name={Name_1}", "Value=Coach", ENDITEM,
+		"Name={Name}", "Value=None", ENDITEM,
+		"Name=outboundFlight", "Value={outboundFlight}", ENDITEM,
+		"Name=advanceDiscount", "Value=0", ENDITEM,
+		"Name=returnFlight", "Value=", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=buyFlights.x", "Value=74", ENDITEM,
+		"Name=buyFlights.y", "Value=9", ENDITEM,
+		"Name=.cgifields", "Value=saveCC", ENDITEM,
+		LAST);
+
+	web_revert_auto_header("Origin");
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"image");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"no-cors");
+
+	web_url("bookanother.gif", 
+		"URL=http://localhost:1080/WebTours/images/bookanother.gif", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Snapshot=t95.inf", 
+		LAST);
+
+	/* home */
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(6);
+
+	web_url("Home Button",
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=menus",
+		"Resource=0",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in={in}",
+		"Snapshot=t96.inf",
+		"Mode=HTTP",
+		LAST);
+
+	web_url("login.pl_3", 
+		"URL=http://localhost:1080/cgi-bin/login.pl?intro=true", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?page=menus", 
+		"Snapshot=t97.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("nav.pl_4", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?page=menus", 
+		"Snapshot=t98.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	/* Sing_off
+	*/
+
+	web_add_header("Sec-Fetch-User", 
+		"?1");
+
+	lr_think_time(6);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t99.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("home.html_2", 
+		"URL=http://localhost:1080/WebTours/home.html", 
+		"Resource=0", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Snapshot=t100.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("nav.pl_5", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Snapshot=t101.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	return 0;
+}
---
+++ DZ#12/UC2/WebSocketBuffer.h
@@ -0,0 +1,10 @@
+#ifndef VUSER_WEBSOCKET_BUFFER_HEADER
+#define VUSER_WEBSOCKET_BUFFER_HEADER
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+char WebSocketReceive0[] = "{\"messageType\":\"hello\",\"uaid\":\"917348c2430b46a194b4a59541f63da0\",\"statu"
+                        "s\":200,\"use_webpush\":true,\"broadcasts\":{}}";
+long WebSocketReceiveLen0   = sizeof(WebSocketReceive0) - 1;	// (record-time: 113 bytes)
+
+#endif
