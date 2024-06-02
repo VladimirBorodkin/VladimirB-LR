6f361ba4956a0228ec6b0cb58efb4a7dad38319c
VladimirBorodkin <162474340+VladimirBorodkin@users.noreply.github.com>
2024-06-02 18:29:49 +0300

    Add files via upload

---
+++ DZ#12/UC_1/Action.c
@@ -0,0 +1,162 @@
+Action()
+{
+lr_start_transaction("UC1_Login");
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Accept-Language", 
+		"en-US,en;q=0.5");
+
+	lr_start_transaction("UC01_Login");
+
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+//	"PROVERKA UC01_1"
+
+	web_reg_find("Text=Welcome to the Web Tours site.",LAST);
+
+	web_custom_request("home.html", 
+		"URL=http://localhost:1080/WebTours/home.html", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Snapshot=t6.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("welcome.pl", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/WebTours/", 
+		"Snapshot=t4.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='138808.611502633HVDQQzHpQftVzzzHtciDQpiAHHcf' Name ='userSession' Type ='Manual'*/
+	web_reg_save_param_ex(
+		"ParamName=userSession",
+		"LB=name=\"userSession\" value=\"",
+		"RB=\"",
+		SEARCH_FILTERS,
+		"Scope=Body",
+		LAST);
+
+	web_url("nav.pl", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Snapshot=t7.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_websocket_send("ID=1", 
+		"Buffer={\"messageType\":\"hello\",\"broadcasts\":{},\"use_webpush\":true}", 
+		"IsBinary=0", 
+		LAST);
+
+	lr_end_transaction("UC01_Login",LR_AUTO);
+
+	lr_think_time(5);
+
+	lr_start_transaction("UC01_02_Login");
+
+
+	/*Connection ID 1 received buffer WebSocketReceive0*/
+
+//	"PROVERKA UC01_2"
+
+	web_reg_find("Fail=Found",
+		"Search=Body",
+		"Text=User password was not correct",
+		LAST);
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t9.inf",
+		"Mode=HTTP",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={username}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=login.x", "Value=40", ENDITEM,
+		"Name=login.y", "Value=8", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+	web_url("login.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/login.pl?intro=true", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t10.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("nav.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t11.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	lr_end_transaction("UC01_02_Login",LR_AUTO);
+
+	lr_think_time(5);
+
+	lr_start_transaction("UC01_03_Exit");
+
+	//	"PROVERKA #UC01_3"
+
+	web_reg_find("Text=The server options can be set via the Admin page",
+		LAST);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t16.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("home.html_2", 
+		"URL=http://localhost:1080/WebTours/home.html", 
+		"Resource=0", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Snapshot=t17.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_url("nav.pl_3", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Snapshot=t18.inf", 
+		"Mode=HTTP", 
+		LAST);
+//	
+	lr_end_transaction("UC01_03_Exit",LR_AUTO);
+
+	lr_think_time(5);
+	
+lr_end_transaction("UC1_Login", LR_AUTO);
+
+	return 0;
+}
